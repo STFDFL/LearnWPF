8cca2a465a659b45f3e3eea0e3c0510277d8c1a9
Stef <stedeflo@hotmail.com>
2017-04-19 06:54:22 +1000

    All functionalities have been implemented, basic design, need polish

--- WpfBasics/DepositCalculator/MainWindow.xaml.cs
+++ WpfBasics/DepositCalculator/MainWindow.xaml.cs
@@ -20,9 +20,100 @@ namespace DepositCalculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        private float _value = 0;
+        private float _funds = 0;
+        private float _weeklySavings = 0;
+        private float _depositPercentage = 0;
+        private bool _firstH = false;
+        private bool _newlyBuilt = false;
+        private bool _imOz = false;
+        private float FHOG = 10000;
+
+        private float _upfrontCost;
+        private float _depositRequired;
+        private float _weeksToGoal;
+
         public MainWindow()
         {
-            //InitializeComponent();
+            InitializeComponent();
+        }
+
+        private void CalculateDepositAndGoal(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                _value = float.Parse(this.PropertyValueBox.Text);
+                _funds = float.Parse(this.SavedFundsBox.Text);
+                _weeklySavings = float.Parse(this.SavingsPerWeekBox.Text);
+
+                CalculateUpfrontCost(_value);
+
+                _depositRequired = (_value / 100) * _depositPercentage;
+                _weeksToGoal = (_upfrontCost + _depositRequired - _funds) / _weeklySavings;
+
+                this.UpfrontCostBox.Text = _upfrontCost.ToString();
+                this.DepositRequiredBox.Text = _depositRequired.ToString();
+                this.WeeksToGoalBox.Text = _weeksToGoal.ToString();
+            }
+            catch
+            {
+
+            }
+        }
+
+        private float CalculateUpfrontCost(float homeValue)
+        {
+            if(_firstH && _newlyBuilt && _imOz )
+            {
+                _upfrontCost = (homeValue / 100) * 4;
+            }
+            else
+            {
+                _upfrontCost = (homeValue / 100) * 9;
+            }
+            return _upfrontCost;
+        }
+
+        private void ImOzCheckbox_Checked(object sender, RoutedEventArgs e)
+        {
+            _imOz = true;
+        }
+
+        private void ImOzCheckbox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            _imOz = false;
+        }
+
+        private void NBCheckbox_Checked(object sender, RoutedEventArgs e)
+        {
+            _newlyBuilt = true;
+        }
+
+        private void NBCheckbox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            _newlyBuilt = false;
+        }
+
+        private void FHCheckbox_Checked(object sender, RoutedEventArgs e)
+        {
+            _firstH = true;
+        }
+
+        private void FHCheckbox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            _firstH = false;
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (CBDeposit.SelectedIndex == 0)
+                _depositPercentage = 5;
+            if (CBDeposit.SelectedIndex == 1)
+                _depositPercentage = 10;
+            if (CBDeposit.SelectedIndex == 2)
+                _depositPercentage = 15;
+            if (CBDeposit.SelectedIndex == 3)
+                _depositPercentage = 20;
         }
     }
 }
